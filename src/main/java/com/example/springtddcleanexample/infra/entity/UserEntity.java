59e3047f3cba4593380ef74e68e3a83d4c99aa4b
Paulo Muniz <paulotadeums@gmail.com>
2023-01-12 09:47:23 -0300

    refactor(user): table settings

--- src/main/java/com/example/springtddcleanexample/infra/entity/UserEntity.java
+++ src/main/java/com/example/springtddcleanexample/infra/entity/UserEntity.java
@@ -2,6 +2,7 @@ package com.example.springtddcleanexample.infra.entity;
 
 import java.io.Serializable;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -24,7 +25,13 @@ public class UserEntity implements Serializable {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
+
+  @Column(length = 45, nullable = false)
   private String name;
+
+  @Column(length = 100, nullable = false, unique = true)
   private String email;
+
+  @Column(length = 12, nullable = false)
   private String password;
 }
